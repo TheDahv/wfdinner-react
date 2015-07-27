1d59fdafd3fa65d4b8047548eb449d8b7d289d33
David Pierce <david.pierce@moz.com>
2015-07-27 12:52:34 -0700

    Adds hover interaction for list item deletion

--- src/components/deletable-list-item.js
+++ src/components/deletable-list-item.js
@@ -1,21 +1,52 @@
 import React from 'react';
+import StyleSheet from 'react-style';
 
 const {PropTypes} = React;
+
+let styles = StyleSheet.create({
+  listItem: {
+    width: '20em'
+  },
+  listItemDelete: {
+    'display': 'none'
+  }
+  , listItemDeleteHover: {
+    'float': 'right',
+    'display': 'inline-block',
+    'textDecoration': 'none'
+  }
+});
 
 export default class DeletableListItem extends React.Component {
   constructor () {
     super();
 
+    this.state = {
+      hover: false
+    };
+
     this._onRequestDelete = this._onRequestDelete.bind(this);
+    this._setHover = this._setHover.bind(this);
+    this._releaseHover = this._releaseHover.bind(this);
   }
+  // className="deletableListItem-delete"
 
   render () {
+    let hoverStyles = [styles.listItemDelete];
+    if (this.state.hover) {
+      hoverStyles.push(styles.listItemDeleteHover);
+    }
+
     return (
-      <li>
+      <li styles={[styles.listItem]}
+          onMouseEnter={this._setHover}
+          onMouseLeave={this._releaseHover} >
+
         {this.props.item}
 
         <a href="#"
-           className="deletableListItem-delete" onClick={this._onRequestDelete}>
+           styles={hoverStyles}
+           onClick={this._onRequestDelete} >
           &#10060;
         </a>
       </li>
@@ -28,6 +59,19 @@ export default class DeletableListItem extends React.Component {
     this.props.onRequestDelete(this.props.index);
   }
 
+  _setHover () {
+    console.log("setting hover");
+    this.setState({
+      hover: true
+    });
+  }
+
+  _releaseHover () {
+    console.log("releasing hover");
+    this.setState({
+      hover: false
+    });
+  }
 };
 
 DeletableListItem.PropTypes = {
