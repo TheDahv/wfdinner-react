7d32c294ed8e2ff6ef7bc34e18f6234eb580fb18
David Pierce <david.pierce@moz.com>
2015-07-27 12:51:59 -0700

    Adds processing/loading for component inline styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var ReactStylePlugin = require('react-style-webpack-plugin');
+
 module.exports = {
   context: __dirname + '/src',
   entry: './app',
@@ -11,8 +13,11 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        loaders: ['jsx', 'babel-loader']
+        loaders: ['jsx', 'babel-loader', ReactStylePlugin.loader()]
       }
     ]
-  }
+  },
+  plugins: [
+    new ReactStylePlugin('bundle.css')
+  ]
 };
